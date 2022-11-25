f3a25a720bea2390b3852e31938b47e4d345c17b
HeikkeB <vldbltn@gmail.com>
2022-11-25 19:12:19 +0300

    fix: fix errors card

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,81 +1,72 @@
 const Card = require('../models/Card');
 
-const { CREATED, OK } = require('../errors/errors');
-const BadRequest = require('../errors/badRequest');
-const NotFound = require('../errors/notFound');
-const ServerError = require('../errors/serverError');
-
-module.exports.createCard = (req, res, next) => {
+module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
-    .then((card) => res.status(CREATED).send({ data: card }))
+    .then((card) => res.status(201).send({ data: card }))
     .catch((err) => {
-      if (err.name === 'CastError') {
-        next(new BadRequest('Incorrect data entered'));
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Incorrect data entered' });
       } else {
-        next(new ServerError('Internal error has occurred'));
+        res.status(500).send({ message: 'Internal error has occurred' });
       }
     });
 };
 
-module.exports.getAllCards = (req, res, next) => {
+module.exports.getAllCards = (req, res) => {
   Card.find({})
-    .then((cards) => res.status(OK).send(cards))
+    .then((cards) => res.status(200).send(cards))
     .catch((err) => {
-      if (err.name === 'CastError') {
-        next(new BadRequest('Incorrect data entered'));
-      } else if (err.name === 'NotFound') {
-        next(new NotFound('Card is not found'));
+      if ((err.name === 'ValidationError') || (err.name === 'CastError')) {
+        res.status(400).send({ message: 'Incorrect data entered' });
       } else {
-        next(new ServerError('Internal error has occurred'));
+        res.status(500).send({ message: 'Internal error has occurred' });
       }
     });
 };
 
-module.exports.removeCard = (req, res, next) => {
+module.exports.removeCard = (req, res) => {
   Card.findByIdAndDelete({ _id: req.params.cardId })
-    .then(() => res.status(OK).send({ message: 'Card deleted' }))
+    .then(() => res.status(200).send({ message: 'Card deleted' }))
     .catch((err) => {
-      if (err.name === 'CastError') {
-        next(new BadRequest('Incorrect data entered'));
-      } else if (err.name === 'NotFound') {
-        next(new NotFound('Card is not found'));
-      } else {
-        next(new ServerError('Internal error has occurred'));
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: 'Incorrect data entered' });
       }
+      return res.status(500).send({ message: 'Internal error has occurred' });
     });
 };
 
-module.exports.addLike = (req, res, next) => {
+module.exports.addLike = (req, res) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $addToSet: { likes: req.user._id } },
     { new: true },
   )
-    .then((card) => res.status(OK).send({ data: card }))
+    .then((card) => res.status(200).send({ data: card }))
     .catch((err) => {
-      if (err.name === 'CastError') {
-        next(new BadRequest('Incorrect data entered'));
+      if ((err.name === 'ValidationError') || (err.name === 'CastError')) {
+        res.status(400).send({ message: 'Incorrect data entered' });
       } else {
-        next(new ServerError('Internal error has occurred'));
+        res.status(500).send({ message: 'Internal error has occurred' });
       }
     });
 };
 
-module.exports.removeLike = (req, res, next) => {
+module.exports.removeLike = (req, res) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $pull: { likes: req.user._id } },
     { new: true },
   )
-    .then((card) => res.status(OK).send({ data: card }))
+    .then((card) => {
+      if (card) res.status(200).send({ data: card });
+      else res.status(404).send({ message: 'Like is not found' });
+    })
     .catch((err) => {
-      if (err.name === 'CastError') {
-        next(new BadRequest('Incorrect data entered'));
-      } else if (err.name === 'NotFound') {
-        next(new NotFound('Like is not found'));
+      if ((err.name === 'ValidationError') || (err.name === 'CastError')) {
+        res.status(400).send({ message: 'Incorrect data entered' });
       } else {
-        next(new ServerError('Internal error has occurred'));
+        res.status(500).send({ message: 'Internal error has occurred' });
       }
     });
 };
